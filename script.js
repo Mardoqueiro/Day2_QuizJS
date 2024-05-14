f1e946b0928e24461a8a7c43207bd5af14e8f673
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-05-14 14:17:07 +0200

    Day 2 Quiz

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = 'Mardoqueiro'
+let lastName = 'Dimbu'
+
+console.log(firstName + lastName)
+
+let numberVar = 5
+
+console.log(numberVar * 5)
+const alt = 'Mordecai'
+alt = 'Dimbu'
+
+// Did you get an error?
+// -Yes, I got an error.
